191ca2280fca8e881ac3132e03476128713478c9
Ra <132078939+RPRd001@users.noreply.github.com>
2024-02-10 20:14:26 -0500

    A new UI/UX was implemented using basic CSS and JS. The user can now play in the body using a select menu and a play button. All the functionality was added to the main.js file

--- main.js
+++ main.js
@@ -1,27 +1,83 @@
+// Initializing buttons and options
+const gameContainer = document.querySelector('.container');
+const playButton = document.querySelector('#play');
+const options = document.querySelector('.options');
+const selectedOption = document.querySelectorAll('.select-option')
+const userOptionImage = document.querySelector('#user-selection-img');
+const computerOptionImage = document.querySelector('#computer-selection-img');
+const resultMessage = document.querySelector('.result');
 
-const choices = ['Rock', 'Paper', 'Scissors'];
+// Adding eventListeners for the Play and Reset buttons
+playButton.addEventListener('click', playRound);
 
-let playerSelection = prompt('Choose - Rock, Paper or Scissors: ');
-let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+// Bank of options for the Computer to randomly select one of them
+const choices = ['rock', 'paper', 'scissors'];
 
+// Computer and Player Selection Variables
+let playerSelection = "";
+let computerSelection = "";
+
+// Changing the user image of the selection to make it correspond to the selected options:
+selectedOption.forEach(option => {
+    option.addEventListener('click', () => {
+        if (option.value == 'paper') {
+            userOptionImage.src = "./img/paper.png"
+        }
+        else if (option.value == 'scissors') {
+            userOptionImage.src = "./img/scissors.png"
+        }
+        else if (option.value == 'rock') {
+            userOptionImage.src = "./img/rock.png"
+        }
+        else {
+            userOptionImage.src = "./img/question.png"        
+        }
+    })
+});
+
+// Game Logic
 function playRound(playerSelection, computerSelection) {
+    /* 
+        NOTE:
+
+        - This lines of code basically capitalize the choices taken by the user
+        and the computer, so when they are displayed in the alert message they
+        are capitalized and therefore nicer to read:
+
+        -! But somehow it didn't work (ERROR: charAt(0) is not a function)
+
+        ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}
+        ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}
+    */
+
+    // Taking the value from the options (select element)
+    playerSelection = options.value;
+    // Taking the value randomly from the choices array
+    computerSelection = choices[Math.floor(Math.random() * choices.length)];
 
     if (playerSelection == computerSelection) {
-        console.log('Drawn! Nobody won!');
+        computerOptionImage.src = `./img/${computerSelection}.png`;
+        gameContainer.setAttribute('style', 'box-shadow: 0px 0px 10px 10px white;')
+        resultMessage.innerText = 'Drawn! Nobody won!';
     }
-    else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-        console.log(`You won! ${playerSelection} beats ${computerSelection}`)
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        computerOptionImage.src = `./img/${computerSelection}.png`;
+        gameContainer.setAttribute('style', 'box-shadow: 0px 0px 10px 10px green;')
+        resultMessage.innerText = `You won! ${playerSelection} beats ${computerSelection}`;
     }
-    else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-        console.log(`You won! ${playerSelection} beats ${computerSelection}`)
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        computerOptionImage.src = `./img/${computerSelection}.png`;
+        gameContainer.setAttribute('style', 'box-shadow: 0px 0px 10px 10px green;')
+        resultMessage.innerText = `You won! ${playerSelection} beats ${computerSelection}`;
     }
-    else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-        console.log(`You won! ${playerSelection} beats ${computerSelection}`)
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        computerOptionImage.src = `./img/${computerSelection}.png`;
+        gameContainer.setAttribute('style', 'box-shadow: 0px 0px 10px 10px green;')
+        resultMessage.innerText = `You won! ${playerSelection} beats ${computerSelection}`;
     }
     else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        computerOptionImage.src = `./img/${computerSelection}.png`;
+        gameContainer.setAttribute('style', 'box-shadow: 0px 0px 10px 10px red;')
+        resultMessage.innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
-
-console.log(playRound(playerSelection, computerSelection))
-console.log("For a new Round, you can refresh this website")
